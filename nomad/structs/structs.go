0bbfa0ac72d63929dae41225530c26473e253f50
JanMa <git@janma.tk>
2019-10-29 22:41:52 +0100

    Patch structs lib

--- nomad/structs/structs.go
+++ nomad/structs/structs.go
@@ -24,7 +24,6 @@ import (
 	"time"
 
 	"github.com/gorhill/cronexpr"
-	hcodec "github.com/hashicorp/go-msgpack/codec"
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/nomad/acl"
@@ -35,6 +34,7 @@ import (
 	psstructs "github.com/hashicorp/nomad/plugins/shared/structs"
 	"github.com/mitchellh/copystructure"
 	"github.com/ugorji/go/codec"
+	hcodec "github.com/ugorji/go/codec"
 	"golang.org/x/crypto/blake2b"
 )
 
